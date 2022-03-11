6d9a2c0a1bfa891e83066cc573da9dd9cab5bb95
Paolo Tessarolo <p.tessarolo@gmail.com>
2022-03-11 21:45:11 +0100

    initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+// tailwind.config.cjs
+const config = {
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [require('@tailwindcss/forms')]
+};
+
+module.exports = config;
